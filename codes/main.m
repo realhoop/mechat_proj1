ce55bfc297421be47ec6255e28afc6538ae3bc8c
Keunhee_Cho <chokh310@gmail.com>
2023-11-13 16:09:41 +0900

    modify main, add test
    modified main to plot the position of the motor

--- codes/main.m
+++ codes/main.m
@@ -7,16 +7,35 @@ Kp = 1; Ki = 1; Kd = 1;  % Replace with your PID constants
 
 pidController = pid(Kp, Ki, Kd);
 
-lastPosition = 0;
-while true
+% Prepare figure for plotting
+hFig = figure('Name', 'Motor Position');
+hPlot = plot(nan, nan);
+ylabel('Position');
+xlabel('Time (s)');
+grid on;
+
+% Variables for time and position
+posData = [];
+timeData = [];
+startTime = tic;  % Start a timer for the x-axis of the plot
+
+while ishandle(hFig)  % Run until the plot window is closed
     currentPosition = readDigitalPin(rpi, encoderPin);
     error = desiredPosition - currentPosition;
     controlSignal = pidController(error);
-    
+
     % Limit control signal to between 0 and 1
     controlSignal = max(0, min(1, controlSignal));
-    
+
     writePWMVoltage(rpi, motorPin, controlSignal);
     
+    % Store data for plotting
+    timeData(end+1) = toc(startTime);
+    posData(end+1) = currentPosition;
+    
+    % Update plot
+    set(hPlot, 'XData', timeData, 'YData', posData);
+    xlim([max(0, timeData(end)-10), timeData(end)+0.1]);  % Show only the last 10 seconds of data
+
     pause(0.01);  % Delay to avoid overwhelming the Raspberry Pi
 end
